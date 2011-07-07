98bbdb483f81f2ddc8b7c3d6a9b9d56e40258160
Katja Karhu <katja.karhu@bitten.fi>
2011-07-07 14:05:44 +0300

    Custom menu & background

--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
 
-//Some simple code for our widget-enabled sidebar
+//Enable sidebar for widgets
 if ( function_exists('register_sidebar') )
     register_sidebar();
 
+//Enable changing background
+add_custom_background();
 
+//Custom menu
+function register_my_menus() {
+  register_nav_menus(
+    array('header-menu' => __( 'Header Menu' ) )
+  );
+}
 
+add_action( 'init', 'register_my_menus' );
 ?>
--- header.php
+++ header.php
@@ -27,6 +27,6 @@
 
 <nav>
 
-<?php wp_nav_menu( ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 
 </nav>
