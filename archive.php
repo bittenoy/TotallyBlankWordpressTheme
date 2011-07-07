d49fb578f543fceaadfa9627c2724f9e4fec4b99
Katja Karhu <katja.karhu@bitten.fi>
2011-07-07 13:41:38 +0300

    Archive templates

---
+++ archive.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<h2><?php the_time() ?></h2>
+
+<?php if (have_posts()) : ?>
+ <?php while (have_posts()) : the_post(); ?>
+<article>
+
+<a href="<?php the_permalink() ?>">
+<?php the_title(); ?>
+</a>
+<br />
+<?php the_excerpt(); ?>
+</article>
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ author.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+ 
+
+<?php if (is_author()) { ?>
+<h2><?php the_author(); ?></h2>
+<?php } ?> 
+ 
+<?php if (have_posts()) : ?>
+ <?php while (have_posts()) : the_post(); ?>
+<article>
+
+<a href="<?php the_permalink() ?>">
+<?php the_title(); ?>
+</a>
+<br />
+<?php the_excerpt(); ?>
+</article>
+
+
+ <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
