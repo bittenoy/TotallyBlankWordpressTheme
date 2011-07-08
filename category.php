148cb0cc4c8a7e04fbae6eb0b5cf749f171ad0d9
Katja Karhu <katja.karhu@bitten.fi>
2011-07-08 22:04:36 +0300

    Added category.php

---
+++ category.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+ 
+<?php if (is_category()) { ?>
+
+	<h2><?php single_cat_title(); ?></h2>
+
+<?php } ?> 
+ 
+<?php if (have_posts()) : ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+
+		<article>
+
+			<a href="<?php the_permalink() ?>">
+			<?php the_title(); ?>
+			</a>
+		
+			<br />
+			<?php the_excerpt(); ?>
+		
+		</article>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
