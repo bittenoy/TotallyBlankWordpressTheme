c3c6aebce7eb8fb25c10cb665dc1e35868d74da0
Katja Karhu <katja.karhu@bitten.fi>
2011-07-09 06:05:52 +0300

    remove_actions

--- functions.php
+++ functions.php
@@ -1,17 +1,30 @@
 <?php
 
+// Remove extra stuff from headers, see: http://wpengineer.com/1438/wordpress-header/
+
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+
 
 // Core functionality stuff
- add_theme_support('automatic-feed-links');
-
+add_theme_support('automatic-feed-links');
 
 
 //Enable sidebar for widgets
 if ( function_exists('register_sidebar') )
     register_sidebar();
 
+
 //Enable changing background
 add_custom_background();
+
 
 //Custom menu(s)
 function register_my_menus() {
@@ -21,7 +34,6 @@ function register_my_menus() {
 }
 
 add_action( 'init', 'register_my_menus' );
-
 
 
 //Custom headers
